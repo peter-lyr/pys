ae3ba50b5360f2ea1a985da9c34bdeaa2e6c8704
peter-lyr <llydrp.ldp@gmail.com>
2024-09-15 22:49:06 +0800

    01-git-clone.py

---
+++ 01-git-clone.py
@@ -0,0 +1,29 @@
+import os
+from multiprocessing import Pool
+
+import funcs as f
+
+
+def clone(cwd, name, repo):
+    cmd = f'cd /d "{cwd}" && git clone git@github.com:{name}/{repo}'
+    print(cmd)
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    params = f.get_params()
+    try:
+        root, name, repos = params[0], params[1], params[2:]
+        if not os.path.exists(root):
+            os.makedirs(root, exist_ok=True)
+        if len(repos) > 1:
+            p = Pool(4)
+            for repo in repos:
+                p.apply_async(clone, args=(root, name, repo))
+            p.close()
+            p.join()
+        else:
+            clone(root, name, repos[0])
+    except Exception as e:
+        print(e)
+    os.system("pause")
---
+++ funcs.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import win32clipboard
+import win32con
+
+
+def get_clipboard_data():
+    win32clipboard.OpenClipboard()
+    try:
+        data = win32clipboard.GetClipboardData(win32con.CF_UNICODETEXT)
+        if data:
+            data = data.replace("\r\n", "\n")
+    except TypeError:
+        data = None
+    win32clipboard.CloseClipboard()
+    return data
+
+
+def get_params_from_file(file):
+    with open(file, "rb") as f:
+        lines = [line.decode("utf-8").strip() for line in f.readlines()]
+    return lines
+
+
+def get_params():
+    params = []
+    if len(sys.argv) > 1:
+        params = sys.argv[1:]
+        if len(params) == 1 and os.path.split(params[0])[1].split("-")[0] == "params":
+            params = get_params_from_file(params[0])
+    else:
+        text = get_clipboard_data()
+        if text != None:
+            params = text.split("\n")
+    return params
