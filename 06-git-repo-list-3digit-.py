3dccafad001eff4b6d6f3286844296c10d747c53
peter-lyr <llydrp.ldp@gmail.com>
2024-09-23 00:07:02 +0800

    "\(.name)\t\t\t\t\t\(.description)"

--- 06-git-repo-list-3digit-.py
+++ 06-git-repo-list-3digit-.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         params = f.get_params()
         root = params[0]
         os.chdir(root)
-        bash_cmd = r"""gh repo list --limit 9999999 --json name,description --jq '.[] | "\(.name)\t\(.description)"'"""
+        bash_cmd = r"""gh repo list --limit 9999999 --json name,description --jq '.[] | "\(.name)\t\t\t\t\t\(.description)"'"""
         result = subprocess.run(
             ["bash.exe", "-c", bash_cmd],
             capture_output=True,
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         repos = result.stdout.replace("\r", "").split("\n")
         R = []
         for repo in repos:
-            if re.match(r"^\d{3}[_-]", repo):
+            # if re.match(r"^\d{3}[_-]", repo):
                 R.append(repo)
         R.sort(reverse=True)
         for r in R:
