15544984420a81ea6bbe55afaa17018cf3b95382
peter-lyr <llydrp.ldp@gmail.com>
2024-09-24 19:23:01 +0800

    05-git-create-submodule.py

--- 05-git-create-submodule.py
+++ 05-git-create-submodule.py
@@ -1,6 +1,5 @@
 import os
 import re
-
 from datetime import datetime
 
 from xpinyin import Pinyin
@@ -15,38 +14,39 @@ if __name__ == "__main__":
         public = params[2]
         name = params[3]
         root_tail = os.path.split(root)[1]
-        if public not in ['public', 'private']:
+        if public not in ["public", "private"]:
             os._exit(3)
         print(root, path)
         if not os.path.exists(root):
             os._exit(1)
         repo = os.path.split(path)[1]
-        repo = (
-            Pinyin().get_initials(repo.replace("-", ":").replace("，", ","))
-            .replace("-", "")
-            .replace(":", "-")
-            .replace(",", "-")
-            .strip()
-        )
+        p = Pinyin()
+        temp = repo.replace("-", ":").replace("，", ",")
+        xxx = p.get_initials(temp)
+        if len(repo) <= 3:
+            xxx = p.get_pinyin(temp)
+        repo = xxx.replace("-", "").replace(":", "-").replace(",", "-").strip()
         repo = re.sub(r"\W+", "_", repo)
         os.chdir(root)
         dir = os.path.join(root, path)
         os.makedirs(dir, exist_ok=True)
         os.chdir(path)
-        if os.path.exists('.git'):
+        if os.path.exists(".git"):
             os._exit(2)
-        os.system('git init')
+        os.system("git init")
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        with open(f'01-初次提交-{timestamp}.txt', 'wb') as f:
-            f.write(b'')
-        os.system('git add .')
+        with open(f"01-初次提交-{timestamp}.txt", "wb") as f:
+            f.write(b"")
+        os.system("git add .")
         os.system('git commit -m "s1"')
-        os.system(f'gh repo create {repo} --{public} --description "{root_tail}/{path}" --source=. --remote=origin')
-        os.system('git push -u origin main')
+        os.system(
+            f'gh repo create {repo} --{public} --description "{root_tail}/{path}" --source=. --remote=origin'
+        )
+        os.system("git push -u origin main")
         os.chdir(root)
-        os.system(f'git submodule add git@github.com:{name}/{repo} {path}')
-        os.system('git add .')
+        os.system(f"git submodule add git@github.com:{name}/{repo} {path}")
+        os.system("git add .")
         os.system(f'git commit -m "{path}"')
-        os.system('git push')
+        os.system("git push")
     except Exception as e:
         print(e, flush=True)
