5b9fb03bbf97986614f6b3f2ed611abcaf284647
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 08:01:14 +0800

    testing

--- 04-git-push-recursive.py
+++ 04-git-push-recursive.py
@@ -55,12 +55,13 @@ if __name__ == "__main__":
                 ] + last_commit_lines
                 with open(CommitFile, "wb") as file:
                     file.writelines(cur_commit_lines)
-            print(f"******* {i+1}. *******", flush=True)
-            print("", flush=True)
-            for line in cur_commit_lines:
-                print(line.rstrip().decode("utf-8"), flush=True)
-            print("", flush=True)
-            print("******************", flush=True)
+            if "commit" in opts:
+                print(f"******* {i+1}. *******", flush=True)
+                print("", flush=True)
+                for line in cur_commit_lines:
+                    print(line.rstrip().decode("utf-8"), flush=True)
+                print("", flush=True)
+                print("******************", flush=True)
             last_commit_lines = cur_commit_lines
             process = subprocess.Popen(
                 cmd,
