15dfec8cb939b61b0dc4878254de4549ba3b54d0
peter-lyr <llydrp.ldp@gmail.com>
2024-10-12 19:57:50 +0800

    测试中文乱码

--- 04-git-push-recursive.py
+++ 04-git-push-recursive.py
@@ -46,6 +46,7 @@ if __name__ == "__main__":
                 "git",
                 "push",
             ]
+        cmd = ["chcp", "&&", "chcp", "65001", "&&"] + cmd
         parent = file
         if os.path.isfile(parent):
             parent = os.path.split(file)[0]
