b5f10bac94588857662029ad8e1ff98017ecaeb9
peter-lyr <llydrp.ldp@gmail.com>
2024-10-12 19:58:40 +0800

    []测试中iii文乱码www[]

--- 04-git-push-recursive.py
+++ 04-git-push-recursive.py
@@ -108,3 +108,5 @@ if __name__ == "__main__":
         e = "wwwwwwwwwwwwwewwwwwwwwwww: " + format_exc()
         print("{{[[{{{1ww}}}]]}}", format_exc(), flush=True)
         b.write_err(e.split("\n"))
+
+
