4db8412da1ac9c4d2ba041cda3664d4fb49d7967
peter-lyr <llydrp.ldp@gmail.com>
2024-11-04 20:23:47 +0800

    if fsize > 0:

--- 04-git-push-recursive.py
+++ 04-git-push-recursive.py
@@ -82,7 +82,8 @@ if __name__ == "__main__":
         last_commit_lines = []
         if Dirs:
             fsize = b.get_untracked_file_size(Dirs[0])
-            p(f'{fsize} untracked files size of:')
+            if fsize > 0:
+                p(f'{fsize} untracked files size of:')
             if fsize > 500 *1024*1024:
                 p(f'{Dirs[0]}\n Is more than 500MB.')
                 os._exit(4)
