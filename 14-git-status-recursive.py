acdf6cf772bfd5e1e03bc07c39a348eb360a858d
peter-lyr <llydrp.ldp@gmail.com>
2024-11-06 23:43:55 +0800

    b.p(repo)

--- 14-git-status-recursive.py
+++ 14-git-status-recursive.py
@@ -14,9 +14,10 @@ for R, D, F in os.walk(root):
         dot_git = os.path.join(d_full, ".git")
         if os.path.exists(dot_git):
             repos.append(d_full)
-for repo in repos:
-    print("=" * len(repo))
-    print(repo)
+l = len(repos)
+for i, repo in enumerate(repos):
+    b.p("# " + str(i + 1) + ". " + "-" * (len(repo) - 5))
+    b.p(repo)
     os.chdir(repo)
     _, output = b.get_sta_output(["git", "status", "-s"])
 # print(output)
