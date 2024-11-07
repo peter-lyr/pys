32e8ec664642bb6fc38440fa13f112fc7a4fd589
peter-lyr <llydrp.ldp@gmail.com>
2024-11-08 00:45:31 +0800

    if os.path.exists(dot_git) and not os.path.exists(dot_gitmodules):

--- 14-git-status-recursive.py
+++ 14-git-status-recursive.py
@@ -7,12 +7,18 @@ import os
 root = sys.argv[1]
 
 # print(root)
-repos = [root]
+dot_git = os.path.join(root, ".git")
+dot_gitmodules = os.path.join(root, ".gitmodules")
+repos = []
+if os.path.exists(dot_git) and not os.path.exists(dot_gitmodules):
+    repos = [root]
 for R, D, F in os.walk(root):
     for d in D:
         d_full = os.path.join(R, d)
         dot_git = os.path.join(d_full, ".git")
-        if os.path.exists(dot_git):
+        dot_gitmodules = os.path.join(d_full, ".gitmodules")
+        if os.path.exists(dot_git) and not os.path.exists(dot_gitmodules):
+            # print(dot_gitmodules)
             repos.append(d_full)
 l = len(repos)
 for i, repo in enumerate(repos):
