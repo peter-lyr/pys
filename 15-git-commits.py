df4c0c82c01ae0c1aebe96c2119408df440e81fe
peter-lyr <llydrp.ldp@gmail.com>
2024-11-08 09:44:52 +0800

    b.p("-> " + output[0])

--- 15-git-commits.py
+++ 15-git-commits.py
@@ -6,17 +6,38 @@ import os
 
 dir = file = sys.argv[1]
 
-if os.path.isfile(file):
+if not os.path.exists(file):
+    os._exit(1)
+elif os.path.isfile(file):
     dir = os.path.dirname(file)
-while 1:
-    if os.path.exists(os.path.join(dir, ".git")):
-        print(dir)
-        os.chdir(dir)
+    while 1:
+        if os.path.exists(os.path.join(dir, ".git")):
+            print(dir)
+            os.chdir(dir)
+            _, output = b.get_sta_output(["git", "rev-list", "--all", "--count"], True)
+            print(output[0])
+        bak = dir
+        dir = os.path.dirname(dir)
+        if bak == dir:
+            break
+elif os.path.isdir(file):
+    # print(dir)
+    dot_git = os.path.join(dir, ".git")
+    repos = []
+    if os.path.exists(dot_git):
+        repos = [dir]
+    for R, D, F in os.walk(dir):
+        for d in D:
+            d_full = os.path.join(R, d)
+            dot_git = os.path.join(d_full, ".git")
+            if os.path.exists(dot_git):
+                repos.append(d_full)
+    l = len(repos)
+    for i, repo in enumerate(repos):
+        os.chdir(repo)
         _, output = b.get_sta_output(["git", "rev-list", "--all", "--count"], True)
-        print(output[0])
-    bak = dir
-    dir = os.path.dirname(dir)
-    if bak == dir:
-        break
+        if output:
+            b.p(repo)
+            b.p("-> " + output[0])
 
 # time.sleep(3)
