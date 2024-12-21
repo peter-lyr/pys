0c0c9c46bbcf1eea29203cbbef2171c8983e0594
peter-lyr <llydrp.ldp@gmail.com>
2024-12-21 15:40:42 +0800

    删除注释

--- 17-work-summary-day.py
+++ 17-work-summary-day.py
@@ -1,25 +1,13 @@
-import os
 import datetime
 import pyperclip
 import re
-import subprocess
 import sys
-import time
-
-import b
 
 work_org = sys.argv[1]
 day = sys.argv[2]
 morning = sys.argv[3]
 
 week_list = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-
-# ptr_task = 0  # * 凯利诺，13X，PS4拔插3.5mm耳机会重新枚举一次
-# ptr_list = 0  # 1. 耳机座4脚增加磁珠，缩短USB线，关闭所有audio寄存器，均未改善。
-# ptr_line = 0
-
-# b.p(work_org)
-# b.p(day)
 
 
 def search_task(lines):
@@ -33,7 +21,6 @@ def search_task(lines):
 
 
 def search_lists(lines):
-    # b.p("\n".join(lines))
     l = []
     percentage = ""
     for line in lines:
@@ -62,7 +49,6 @@ def search_lists(lines):
 if __name__ == "__main__":
     with open(work_org, "rb") as f:
         lines = [line.strip().decode("utf-8") for line in f.readlines()]
-    # tasks = []
     tasks = {}
     y, m, d = [int(i) for i in day.split("-")]
     date_week = datetime.date(y, m, d).weekday()
@@ -70,17 +56,10 @@ if __name__ == "__main__":
     task_cnt = 1
     for i, line in enumerate(lines):
         if re.findall(r"^\*\* " + day, line):  # ** ~2024-12-19这种不汇报
-            # b.p(line.strip())
-            # b.p(str(i) + "-----------")
-            # tasks.append(search_task(lines[i::-1]))
             task = search_task(lines[i::-1])
-            # b.p("task:")
-            # b.p(task)
             if not task:
                 continue
             lists = search_lists(lines[i + 1 : :])
-            # b.p("lists:")
-            # b.p("\n".join(lists))
             if not lists:
                 tasks[task] = "未跟进"
                 if morning == "morning":
@@ -95,6 +74,4 @@ if __name__ == "__main__":
                 else:
                     text += f"{task_cnt}. {task}->{t}\n"
             task_cnt += 1
-    # b.p("tasks:")
-    # b.p(str(tasks))
     pyperclip.copy(text.strip())
