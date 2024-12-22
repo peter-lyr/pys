f7d81330c3e564713094c1d9a169d5d96447cd62
peter-lyr <llydrp.ldp@gmail.com>
2024-12-22 15:36:28 +0800

    s1

--- 05-git-create-submodule.py
+++ 05-git-create-submodule.py
@@ -33,13 +33,13 @@ def get_multi_repo(repo):
     # 等等
     temp_l = repo.split("，")
     temp_a = get_permutations(temp_l)
-    b.p(str(temp_a))
+    # b.p(str(temp_a))
     if not temp_a:
         return []
     temp_a = list(temp_a)
     temp_b = []
     for i in temp_a:
-        b.p(str(i) + "=-=========" + repo)
+        # b.p(str(i) + "=-=========" + repo)
         if i != repo:
             temp_b.append(i)
     return ["，".join(i) for i in list(temp_b)]
@@ -105,25 +105,25 @@ if __name__ == "__main__":
         os.makedirs(dir, exist_ok=True)
         os.chdir(path)
         if os.path.exists(".git"):
-            print(f".git exists in {path}", flush=True)
+            b.p(f".git exists in {path}")
             os._exit(2)
         repos = git_repo_list_3digit.get_all_repos()
         repo_exists = 0
         for r in repos:
             res = re.findall(repo_bak, r)
-            b.p(f"{repo_bak} {r}")
+            # b.p(f"{repo_bak} {r}")
             if res:
                 repo_exists = 1
                 break
         if not repo_exists:  # 不存在仓库
             multi_repo_names = get_multi_repo(repo_bak_ori)
-            b.p(str(multi_repo_names))
+            # b.p(str(multi_repo_names))
             to_break = 0
             for r in repos:
                 for bak in multi_repo_names:
                     bak = get_w_repo(bak)
                     res = re.findall(bak, r)
-                    b.p(f"{bak} {r}")
+                    # b.p(f"{bak} {r}")
                     if res:
                         repo_exists = 1
                         temp__ = get_repo_pinyin(
