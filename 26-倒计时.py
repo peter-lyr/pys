bb077573d9da4628f7c238c6cc0239abd9aa7202
peter-lyr <llydrp.ldp@gmail.com>
2025-08-20 11:41:59 +0800

    26-倒计时.py

---
+++ 26-倒计时.py
@@ -0,0 +1,15 @@
+import time
+L = 60*15
+for i in range(L):
+    time.sleep(1)
+    print(f'{i}s/{L} {round(i/60, 3)}m/{int(L/60)}')
+
+import tkinter as tk
+# from tkinter import messagebox
+window = tk.Tk()
+window.title('主窗口标题')
+window.geometry('3840x1080')
+# def display_messagebox():
+#     messagebox.showinfo('弹窗标题', '这是弹窗显示的信息\n这是弹窗显示的信息\n')
+# tk.Button(window, text='显示弹窗', command=display_messagebox).pack()
+window.mainloop()
