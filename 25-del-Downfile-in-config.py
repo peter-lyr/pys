27a03bc5d6879297208cefceb7bcd5fe4f837aa5
peter-lyr <llydrp.ldp@gmail.com>
2025-08-20 11:30:21 +0800

    25-del-Downfile-in-config.py

---
+++ 25-del-Downfile-in-config.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+def main():
+    with open('Downloader.config', 'rb') as f:
+        lines = f.readlines()
+    L = []
+    C = 0
+    for line in lines:
+        res = re.findall(re.compile(r'key="DownFile\d*" value="([^"]+)"'), line.decode('utf-8'))
+        if res:
+            if os.path.exists(res[0]):
+                a = C
+                if C == 0:
+                    a = ''
+                line = f'        <add key="DownFile{a}" value="{res[0]}" />\r\n'
+                C += 1
+                L.append(line.encode('utf-8'))
+        else:
+            L.append(line)
+    with open('Downloader.config', 'wb') as f:
+        f.writelines(L)
+
+if __name__ == "__main__":
+    main()
